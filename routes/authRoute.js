40922f023f5f9c835b791ee5fd547abce56ef244
lebin25 <lebin642@gmail.com>
2023-09-14 22:14:55 +0700

    Update Password, Forgot Password & send Email, Reset Password

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -9,11 +9,19 @@ const {
     blockUser,
     unblockUser,
     handleRefreshToken,
-    logout
+    logout,
+    updatePassword,
+    forgotPasswordToken,
+    resetPassword,
 } = require('../controller/userCtrl');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 const router = express.Router();
+
 router.post('/register', createUser);
+router.post('/forgot-password-token', forgotPasswordToken);
+router.put('/reset-password/:token', resetPassword);
+
+router.put('/password', authMiddleware, updatePassword);
 router.post('/login', loginUserCtrl);
 router.get('/all-users', getAllUser);
 router.get('/refresh', handleRefreshToken);
@@ -21,6 +29,7 @@ router.get('/logout', logout);
 router.get('/:id', authMiddleware, isAdmin, getUserById);
 router.delete('/:id', deleteUserById);
 router.put('/edit-user', authMiddleware, updateUserById);
+
 router.put('/block-user/:id', authMiddleware, isAdmin, blockUser);
 router.put('/unblock-user/:id', authMiddleware, isAdmin, unblockUser);
 
