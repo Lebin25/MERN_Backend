0406483fa4342ddb62e80564972546dc945e4052
lebin25 <lebin642@gmail.com>
2023-09-14 14:42:20 +0700

    ProductRouter, Sort, Filter, Pagination

---
+++ routes/productRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { 
+    createProduct, 
+    getProductById, 
+    getAllProduct,
+    updateProduct,
+    deleteProduct,
+} = require("../controller/productCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+const router = express.Router();
+
+router.post('/', authMiddleware, isAdmin, createProduct);
+router.get('/:id', getProductById);
+router.put('/:id', authMiddleware, isAdmin, updateProduct);
+router.delete('/:id', authMiddleware, isAdmin, deleteProduct);
+router.get('/', getAllProduct);
+
+module.exports = router;
