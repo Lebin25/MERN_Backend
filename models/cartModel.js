b66ba9a3d50cc59eb1264a5e668f7f39a25a43fe
lebin25 <lebin642@gmail.com>
2023-11-01 02:15:31 +0700

    UpdateWithFronend/AddProductToCart-FetchCartProducts

--- models/cartModel.js
+++ models/cartModel.js
@@ -3,22 +3,25 @@ const mongoose = require("mongoose"); // Erase if already required
 // Declare the Schema of the Mongo model
 var cartSchema = new mongoose.Schema(
    {
-      products: [
-         {
-            product: {
-               type: mongoose.Schema.Types.ObjectId,
-               ref: "Product",
-            },
-            count: Number,
-            color: String,
-            price: Number,
-         },
-      ],
-      cartTotal: Number,
-      totalAfterDiscount: Number,
-      orderby: {
+      userId: {
          type: mongoose.Schema.Types.ObjectId,
-         ref: "User",
+         ref: "User"
+      },
+      productId: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Product"
+      },
+      quantity: {
+         type: Number,
+         require: true
+      },
+      price: {
+         type: Number,
+         require: true
+      },
+      color: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Color"
       },
    },
    {
