e1a9de41bdf61dae2894b6c2af880fa42e545dff
lebin25 <lebin642@gmail.com>
2023-11-08 02:18:24 +0700

    Checkout&RazorPayment

--- models/orderModel.js
+++ models/orderModel.js
@@ -3,33 +3,89 @@ const mongoose = require("mongoose"); // Erase if already required
 // Declare the Schema of the Mongo model
 var orderSchema = new mongoose.Schema(
    {
-      products: [
+      user: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User",
+         require: true
+      },
+      shippingInfo: {
+         firstName: {
+            type: String,
+            require: true
+         },
+         lastName: {
+            type: String,
+            require: true
+         },
+         address: {
+            type: String,
+            require: true
+         },
+         city: {
+            type: String,
+            require: true
+         },
+         state: {
+            type: String,
+            require: true
+         },
+         other: {
+            type: String,
+            require: true
+         },
+         pincode: {
+            type: Number,
+            require: true
+         },
+      },
+      paymentInfo: {
+         razorpayOrderId: {
+            type: String,
+            require: true
+         },
+         razorpayPaymentId: {
+            type: String,
+            require: true
+         },
+      },
+      orderItem: [
          {
             product: {
                type: mongoose.Schema.Types.ObjectId,
                ref: "Product",
+               require: true
             },
-            count: Number,
-            color: String,
-         },
+            color: {
+               type: mongoose.Schema.Types.ObjectId,
+               ref: "Color",
+               require: true
+            },
+            quantity: {
+               type: Number,
+               require: true
+            },
+            price: {
+               type: Number,
+               require: true
+            },
+         }
       ],
-      paymentIntent: {},
+      paidAt: {
+         type: Date,
+         default: Date.now()
+      },
+      totalPrice: {
+         type: Number,
+         require: true
+      },
+      totalPriceAfterDiscount: {
+         type: Number,
+         require: true
+      },
       orderStatus: {
          type: String,
-         default: "Not Processed",
-         enum: [
-            "Not Processed",
-            "Cash on Delivery",
-            "Processing",
-            "Dispatched",
-            "Cancelled",
-            "Delivered",
-         ],
-      },
-      orderby: {
-         type: mongoose.Schema.Types.ObjectId,
-         ref: "User",
-      },
+         default: "Ordered"
+      }
    },
    {
       timestamps: true,
