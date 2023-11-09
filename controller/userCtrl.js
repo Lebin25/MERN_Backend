39bd4b4b5fa5b2abc3e511a6da50e964e5dd5b33
lebin25 <lebin642@gmail.com>
2023-11-09 14:26:32 +0700

    update

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -314,6 +314,7 @@ const forgotPasswordToken = asyncHandler(async (req, res) => {
 const resetPassword = asyncHandler(async (req, res) => {
    const { password } = req.body;
    const { token } = req.params;
+   console.log(token);
    const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
    const user = await User.findOne({
       passwordResetToken: hashedToken,
